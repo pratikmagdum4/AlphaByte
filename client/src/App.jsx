020a3e0b55bf4b48d12681336e5d41e3cb011c39
Pratik Magdum <137306806+pratikmagdum4@users.noreply.github.com>
2024-03-16 22:08:09 +0530

    Update App.jsx
    login and signup links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
-import NavBar from './components/NavBar'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Login from './components/Login';
+import AdminLogin from './components/AdminLogin';
+import CandidateLogin from './components/CandidateLogin';
+import AdminSignup from './components/AdminSignup'; // Import the AdminSignup component
+import CandidateSignup from './components/CandidateSignup'; // Import the CandidateSignup component
+import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
   return (
-    <NavBar/>
-    
-  )
+    <BrowserRouter>
+      <div>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/admin-login" element={<AdminLogin />} />
+          <Route path="/candidate-login" element={<CandidateLogin />} />
+          <Route path="/admin-Signup" element={<AdminSignup />} /> {/* Route to the AdminSignup component */}
+          <Route path="/candidate-Signup" element={<CandidateSignup />} /> {/* Route to the CandidateSignup component */}
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
